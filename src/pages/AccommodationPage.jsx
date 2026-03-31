367a2608fe556be92c7e248bf47cf839f2334fb2
Maud Tapia <maud.tapia@gmail.com>
2026-03-31 22:07:50 +0200

    Gestion erreur si Id logement incorrect

--- src/pages/AccommodationPage.jsx
+++ src/pages/AccommodationPage.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 import data from "../datas/logements.json"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
@@ -13,61 +14,65 @@ function Accommodation() {
 
     return (
         <div>
-            <div className="main-container">
-                <Header/>
-                <div className="accommodation">
-                    <SlideShow slides={dataLogements.pictures}/>
-                        <div className="fiche">
-                            <section className="fiche__section">
-                                <div className="fiche__section__top">
-                                    <div className="titlesAndTags">
-                                        <div className="titlesAndTags__titles">
-                                            <h1>{dataLogements.title}</h1>
-                                            <h2>{dataLogements.location}</h2>
+            {dataLogements ? (
+                <div>
+                    <div className="main-container">
+                    <Header/>
+                    <div className="accommodation">
+                        <SlideShow slides={dataLogements.pictures}/>
+                            <div className="fiche">
+                                <section className="fiche__section">
+                                    <div className="fiche__section__top">
+                                        <div className="titlesAndTags">
+                                            <div className="titlesAndTags__titles">
+                                                <h1>{dataLogements.title}</h1>
+                                                <h2>{dataLogements.location}</h2>
+                                            </div>
+                                        <div className="titlesAndTags__tags">
+                                            {dataLogements.tags.map((tag, i) => (
+                                                <span key={i} className="titlesAndTags__tags__button">{tag}</span>
+                                            ))}
                                         </div>
-                                    <div className="titlesAndTags__tags">
-                                        {dataLogements.tags.map((tag, i) => (
-                                            <span key={i} className="titlesAndTags__tags__button">{tag}</span>
-                                        ))}
-                                    </div>
-                                </div>         
-                                <div className="HostAndRating">
-                                    <div className="HostAndRating__host">
-                                        <p>{dataLogements.host.name}</p>
-                                        <img src={dataLogements.host.picture} alt="Photo du propriétaire"></img>
-                                    </div>
-                                    <div className="HostAndRating__rating">
-                                        <Rate score={dataLogements.rating} />
+                                    </div>         
+                                    <div className="HostAndRating">
+                                        <div className="HostAndRating__host">
+                                            <p>{dataLogements.host.name}</p>
+                                            <img src={dataLogements.host.picture} alt="Photo du propriétaire"></img>
+                                        </div>
+                                        <div className="HostAndRating__rating">
+                                            <Rate score={dataLogements.rating} />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </section>
-                        <section className="fiche__section">
-                            <div className="fiche__section__bottom">
-                                <div className="fiche__section__bottom__collapse fiche__section__bottom__collapse__description">
-                                    <Collapse 
-                                        title="Description"
-                                        description={dataLogements.description}
-                                    />
+                            </section>
+                            <section className="fiche__section">
+                                <div className="fiche__section__bottom">
+                                    <div className="fiche__section__bottom__collapse fiche__section__bottom__collapse__description">
+                                        <Collapse 
+                                            title="Description"
+                                            description={dataLogements.description}
+                                        />
+                                    </div>
+                                    <div className="fiche__section__bottom__collapse">
+                                        <Collapse
+                                            title="Equipements"
+                                            description={
+                                                dataLogements.equipments.map((equipements, i) => (
+                                                    <ul key={i}>
+                                                        <li>{equipements}</li>    
+                                                    </ul> 
+                                                ))
+                                            }
+                                        />
+                                    </div>
                                 </div>
-                                <div className="fiche__section__bottom__collapse">
-                                    <Collapse
-                                        title="Equipements"
-                                        description={
-                                            dataLogements.equipments.map((equipements, i) => (
-                                                <ul key={i}>
-                                                    <li>{equipements}</li>    
-                                                </ul> 
-                                            ))
-                                        }
-                                    />
-                                </div>
-                            </div>
-                        </section>
-                    </div>              
+                            </section>
+                        </div>              
+                    </div>
                 </div>
+                <Footer />
             </div>
-            <Footer />
+            ) : (<Navigate replace to="/notFound"/>)}
         </div>
     )
 }
